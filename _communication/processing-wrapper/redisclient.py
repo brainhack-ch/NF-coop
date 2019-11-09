2e37b50d3ea8616f68d6d6ccaaf0d42849e0d793
Matt Clarke <mclarke0805@gmail.com>
2019-11-09 13:54:07 +0100

    Update redisclient.py

--- _communication/processing-wrapper/redisclient.py
+++ _communication/processing-wrapper/redisclient.py
@@ -46,7 +46,7 @@ class RedisClient:
         return 0
 
     def map_demo_headset_name_to_queue(self, name):
-        if name == 'WS-default':
+        if name == 'P04':
             return 'player_0'
         elif name == 'unknown2':
             return 'player_1'
